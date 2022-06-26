d054629aae039bce37c172e088d8e96e2587a975
Pmoonesi <parham.moonesi@outlook.com>
2022-06-26 16:47:09 +0430

    add different next generation selection strategies

--- evolution.py
+++ evolution.py
@@ -1,11 +1,24 @@
 import copy
 
 from player import Player
-
+import numpy as np
 
 class Evolution:
     def __init__(self):
         self.game_mode = "Neuroevolution"
+
+    def binary_find(self, array, x, start, end):
+        if end <= start:
+            return start
+        mid = int((start + end)/2)
+        if mid == 0:
+            return 0
+        if x >= array[mid-1] and x < array[mid]:
+            return mid
+        elif array[mid] <= x:
+            return self.binary_find(array, x, mid + 1, end)
+        else:
+            return self.binary_find(array, x, start, mid - 1)
 
     def next_population_selection(self, players, num_players):
         """
@@ -16,8 +29,47 @@ class Evolution:
         :param num_players: number of players that we return
         """
         # TODO (Implement top-k algorithm here)
+        for i in range(num_players):
+            max = -float('inf')
+            max_index = -1
+            for j in range(i, len(players)):
+                player = players[j]
+                if player.fitness > max:
+                    max = player.fitness
+                    max_index = j
+            temp = players[i]
+            players[i] = players[max_index]
+            players[max_index] = temp
+        return players[: num_players]
+
+        ## create chance array
+        # prefix_fitness = [players[0].fitness]
+        # for i in range(1, len(players)):
+        #     new_fitness_sum = prefix_fitness[-1] + players[i].fitness
+        #     prefix_fitness.append(new_fitness_sum)
+        # total_fitness = prefix_fitness[-1]
+        # prefix_fitness = [f / total_fitness for f in prefix_fitness]
+
         # TODO (Additional: Implement roulette wheel here)
+        ## generate num_players uniform random numbers and select next generation
+        # selected_players = []
+        # for i in range(num_players):
+        #     random_number = np.random.uniform()
+        #     index = self.binary_find(prefix_fitness, random_number, 0, len(prefix_fitness))
+        #     selected_players.append(players[index])
+        # return selected_players
+
         # TODO (Additional: Implement SUS here)
+        # selected_players = []
+        # interval = 1 / num_players
+        # random_var = np.random.uniform(0, interval)
+        # index = 0
+        # while random_var < 1:
+        #     while random_var > prefix_fitness[index]:
+        #         index += 1
+        #     selected_players.append(players[index])
+        #     random_var += interval
+        # return selected_players
 
         # TODO (Additional: Learning curve)
         return players[: num_players]
